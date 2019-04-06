3965b972dbadbfc40d397c8bd0f83322b5645ba3
general <generals.space@gmail.com>
2019-04-06 20:01:22 +0800

    拆分文件

--- model/asset_task.go
+++ model/asset_task.go
@@ -2,22 +2,6 @@ package model
 
 import "github.com/jinzhu/gorm"
 
-// QueryPageTasks 查询页面任务队列记录, 同时删除所有记录(之后要加载到任务队列中).
-func QueryPageTasks(db *gorm.DB) (tasks []*PageTask, err error) {
-	tasks = []*PageTask{}
-	err = db.Find(&tasks).Error
-	if err != nil {
-		if err.Error() == "record not found" {
-			err = nil
-		}
-	}
-	err = db.Unscoped().Delete(&PageTask{}).Error
-	if err != nil {
-		logger.Errorf("清空页面任务队列记录失败: %s", err.Error())
-	}
-	return
-}
-
 // QueryAssetTasks 查询静态资源任务队列记录, 同时删除所有记录(之后要加载到任务队列中).
 func QueryAssetTasks(db *gorm.DB) (tasks []*AssetTask, err error) {
 	tasks = []*AssetTask{}
@@ -34,19 +18,6 @@ func QueryAssetTasks(db *gorm.DB) (tasks []*AssetTask, err error) {
 	return
 }
 
-// AddPageTask 添加页面任务到数据库
-func AddPageTask(db *gorm.DB, req *URLTask) (err error) {
-	pageTaskModel := &PageTask{
-		URLTask: *req,
-	}
-	err = db.Create(pageTaskModel).Error
-	if err != nil {
-		logger.Errorf("添加页面任务队列失败: req: %+v, error: %s", req, err.Error())
-		return
-	}
-	return
-}
-
 // AddAssetTask 添加静态资源任务到数据库
 func AddAssetTask(db *gorm.DB, req *URLTask) (err error) {
 	assetTaskModel := &AssetTask{
@@ -55,16 +26,6 @@ func AddAssetTask(db *gorm.DB, req *URLTask) (err error) {
 	err = db.Create(assetTaskModel).Error
 	if err != nil {
 		logger.Errorf("添加静态资源任务队列失败: req: %+v, error: %s", req, err.Error())
-		return
-	}
-	return
-}
-
-// DelPageTask 删除指定任务
-func DelPageTask(db *gorm.DB, req *URLTask) (err error) {
-	err = db.Unscoped().Where("url = ?", req.URL).Delete(&PageTask{}).Error
-	if err != nil {
-		logger.Errorf("删除页面任务队列记录失败: req: %+v, error: %s", req, err.Error())
 		return
 	}
 	return
